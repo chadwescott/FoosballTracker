3b9207ee890a9276ee3e0ec7f46e99ee0d669973
Chad Wescott <cwescott@coopervision.com>
2015-11-23 11:29:10 -0500

    More results grid improvements.

--- Foosball.Web/Models/PlayerResultsViewModel.cs
+++ Foosball.Web/Models/PlayerResultsViewModel.cs
@@ -8,11 +8,15 @@ namespace Foosball.Web.Models
 
         public Guid PlayerId { get; set; }
 
+        private double PlayerScore { get { return PlayerId == WinnerId ? WinnerScore : LoserScore; } }
+
         public IPlayer Opponent { get { return PlayerId == WinnerId ? Loser : Winner; } }
+
+        private double OpponentScore { get { return PlayerId == WinnerId ? LoserScore : WinnerScore; } }
 
         public string Score
         {
-            get { return string.Format("({0} - {1})", WinnerScore, LoserScore); }
+            get { return string.Format("{0} - {1}", PlayerScore, OpponentScore); }
         }
 
         public double Rating { get { return PlayerId == WinnerId ? WinnerRating : LoserRating; } }
